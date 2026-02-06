b3c0ff4c03988a05958d0a60628c66cb8e1a27f6
Harikrishnan Santhosh <harikrishnansanthosh06@gmail.com>
2026-02-06 10:54:25 +0530

    imple : mdl

--- src/InterfaceResume.java
+++ src/InterfaceResume.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 
 interface Resume {
-    // TODO: Declare method void biodata();
+    void biodata();
 }
 
 class Teacher implements Resume {
-    // Attributes
-   
+    String name;
+    String qualification;
+    int experience;
 
-    // TODO: Implement biodata() method to print details in the required format
+    public void biodata() {
+        System.out.println("Name: " + name);
+        System.out.println("Qualification: " + qualification);
+        System.out.println("Experience: " + experience + " years");
+    }
 }
 
 public class InterfaceResume {
-   
-        
-        // TODO: Read input (Name, Qualification, Experience)
-        // Assign to teacher object attributes
-        
-        // TODO: Call teacher.biodata()
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Teacher teacher = new Teacher();
+        teacher.name = sc.nextLine();
+        teacher.qualification = sc.nextLine();
+        teacher.experience = sc.nextInt();
+        teacher.biodata();
+    }
 }
